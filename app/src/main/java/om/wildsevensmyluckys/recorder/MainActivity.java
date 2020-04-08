1c120323e130c6d161c05e10411d9cac53da5798
denis <025896321Denis>
2020-04-08 16:01:23 +0300

    make crezte new records

--- app/src/main/java/om/wildsevensmyluckys/recorder/MainActivity.java
+++ app/src/main/java/om/wildsevensmyluckys/recorder/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,21 +21,26 @@ import android.widget.Button;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "SoundRecordingDemo";
 
     private Button mStartRecordingButton;
     private Button mStopRecordingButton;
-    private MediaRecorder mMediaRecorder;
-    private File mAudioFile;
+    private MediaRecorder mediaRecorder;
+    private File fileName;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMediaRecorder = new MediaRecorder();
+        mediaRecorder = new MediaRecorder();
 
         mStartRecordingButton = findViewById(R.id.record_button);
         mStopRecordingButton = findViewById(R.id.stop_button);
@@ -53,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                         String[] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
                         ActivityCompat.requestPermissions(MainActivity.this, permissions, 0);
                     } else {
-                        startRecording();
+                       recordStart();
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -67,8 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 mStartRecordingButton.setEnabled(true);
                 mStopRecordingButton.setEnabled(false);
                 mStartRecordingButton.requestFocus();
-                stopRecording();
-                processAudioFile();
+                recordStop();
             }
         });
 
@@ -76,50 +81,98 @@ public class MainActivity extends AppCompatActivity {
         mStartRecordingButton.setEnabled(true);
     }
 
-    private void processAudioFile() {
-        ContentValues values = new ContentValues(4);
-        long current = System.currentTimeMillis();
 
-        values.put(MediaStore.Audio.Media.TITLE, "audio" + mAudioFile.getName());
-        values.put(MediaStore.Audio.Media.DATE_ADDED, (int) (current / 1000));
-        values.put(MediaStore.Audio.Media.MIME_TYPE, "audio/3gpp");
-        values.put(MediaStore.Audio.Media.DATA, mAudioFile.getAbsolutePath());
-        ContentResolver contentResolver = getContentResolver();
-
-        Uri baseUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        Uri newUri = contentResolver.insert(baseUri, values);
-
-        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, newUri));
-    }
-
-    private void startRecording() throws IOException {
-        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        if (mAudioFile == null) {
+    public void recordStart() {
+        if (fileName == null) {
             try {
-            File sampleDir = new File(
-                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC), getPackageName() + getResources().getString(R.string.app_name));
-            if (!sampleDir.exists()) {
-                sampleDir.mkdirs();
-            }
+                File sampleDir = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC), getPackageName() + getResources().getString(R.string.app_name));
+                if (!sampleDir.exists()) {
+                    sampleDir.mkdirs();
+                }
+
+                // Текущее время
+                Date currentDate = new Date();
+// Форматирование времени как "день.месяц.год"
+                DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+                String dateText = dateFormat.format(currentDate);
+// Форматирование времени как "часы:минуты:секунды"
+                DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+                String timeText = timeFormat.format(currentDate);
 
 
-                mAudioFile = File.createTempFile("meow", ".3gp", sampleDir);
+                fileName = File.createTempFile(String.format("meow %s %s ", dateText, timeText), ".3gp", sampleDir);
             } catch (IOException e) {
 
                 Log.e(TAG, "sdcard access error");
                 return;
             }
         }
-        mMediaRecorder.setOutputFile(mAudioFile.getAbsolutePath());
 
-        mMediaRecorder.prepare();
-        mMediaRecorder.start();
+        try {
+            releaseRecorder();
+
+            if (fileName.exists()) {
+                fileName.delete();
+            }
+
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile(fileName.getPath());
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
-    private void stopRecording() {
-        mMediaRecorder.stop();
-        mMediaRecorder.release();
+    public void recordStop() {
+        if (mediaRecorder != null) {
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            fileName = null;
+        }
+    }
+
+    public void playStart(View v) {
+        try {
+            releasePlayer();
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(fileName.getPath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void playStop(View v) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+    private void releaseRecorder() {
+        if (mediaRecorder != null) {
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
+    }
+
+    private void releasePlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+        releaseRecorder();
     }
 }
